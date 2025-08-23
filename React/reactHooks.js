6d29b9c06581d66b57e7fad71896128f4b9c7d2d
Aditi Shukla <aditishukla1676@gmail.com>
2025-08-23 23:34:15 +0530

    hooks

---
+++ React/reactHooks.js
@@ -0,0 +1,18 @@
+//useState
+const ToggleMessage = () => {
+  let [notificationCount, setNotificationCount] = useState(0);
+
+  console.log("re-render");
+  function increment() {
+    setNotificationCount(notificationCount + 1);
+  }
+
+  return (
+      <div>
+          <button onClick={increment}>
+              Increase count
+          </button>
+          {notificationCount}
+      </div>
+  );
+};
